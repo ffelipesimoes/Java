de36e1e606da42aa353077a20ae1af70a233ef98
ffelipesimoes <ffelipesimoes@users.noreply.github.com>
2019-03-13 23:23:23 -0300

    Adding Mining

--- new-eclipse/noobChain/src/noobChain/Block.java
+++ new-eclipse/noobChain/src/noobChain/Block.java
@@ -1,30 +1,41 @@
 package noobChain;
-
 import java.util.Date;
 
+
 public class Block {
+	public static int difficulty = 5;
 	public String hash;
-	public String previousHash;
-	private String data;
-	private long timeStamp;
+	public String previousHash; 
+	private String data; //our data will be a simple message.
+	private long timeStamp; //as number of milliseconds since 1/1/1970.
+	private int nonce;
 	
-	
-	public Block() {
-		
-	}
-	public Block(String data, String previoushash) {
+	//Block Constructor.  
+	public Block(String data,String previousHash ) {
 		this.data = data;
-		this.previousHash = previoushash;
+		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
-		this.hash = calculateHash();
+		
+		this.hash = calculateHash(); //Making sure we do this after we set the other values.
 	}
 	
+	//Calculate new hash based on blocks contents
 	public String calculateHash() {
-		String calculateHash = StringUtil.applySha256(
+		String calculatedhash = StringUtil.applySha256( 
 				previousHash +
 				Long.toString(timeStamp) +
-				data
+				Integer.toString(nonce) + 
+				data 
 				);
-		return calculateHash;
+		return calculatedhash;
+	}
+	
+	public void mineBlock(int difficulty) {
+		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
 	}
 }
